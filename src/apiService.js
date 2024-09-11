bb738c964edf724acea369cf24d2812239105483
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-11 07:17:18 +0300

    Added apiService.js

---
+++ src/apiService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/siz-items";
+
+// Получение всех записей СИЗ
+export const getSIZItems = () => axios.get(API_URL);
+
+// Создание новой записи СИЗ
+export const createSIZItem = (item) => axios.post(API_URL, item);
+
+// Обновление записи СИЗ
+export const updateSIZItem = (id, updatedItem) =>
+  axios.put(`${API_URL}/${id}`, updatedItem);
+
+// Удаление записи СИЗ
+export const deleteSIZItem = (id) => axios.delete(`${API_URL}/${id}`);
