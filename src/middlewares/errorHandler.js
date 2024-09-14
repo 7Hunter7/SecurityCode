81305f2b8887028a7e92367c77791c76decc9a84
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-14 14:47:37 +0300

    Upgrated 'errorHandler.js' - Added Joi validation error handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,30 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack); // Логирование ошибки
 
-  // Если ошибка имеет статус-код, то используем его, иначе ставим 500
-  const statusCode = err.statusCode || 500;
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Внутренняя ошибка сервера";
 
-  // Если статус-код 500, мы возвращаем сообщение "Internal Server Error" для скрытия деталей
-  const message =
-    statusCode === 500 ? "Внутренняя ошибка сервера" : err.message;
+  // Обработка ошибок валидации Joi
+  if (err.isJoi) {
+    statusCode = 400;
+    message = err.details[0].message;
+  }
+
+  // Обработка ошибок валидации Mongoose
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+    message = "Ошибка валидации данных";
+  }
+
+  // Обработка ошибок дублирования ключей
+  if (err.code && err.code === 11000) {
+    statusCode = 400;
+    message = "Дублирование уникального поля";
+  }
+
+  if (statusCode === 500) {
+    message = "Внутренняя ошибка сервера";
+  }
 
   res.status(statusCode).json({
     status: "error",
