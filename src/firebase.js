ff14684afc8786fa79ea3efa360c8b7965852a02
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-15 17:19:12 +0300

    Updated 'firebase.js'

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); //  Экземпляр хранилища Firestore
 const auth = getAuth(app); // Аутентификация в Firebase
+const analytics = getAnalytics(app); // Аналитика Firebase
 
-export { db, auth };
+export { db, auth, analytics };
