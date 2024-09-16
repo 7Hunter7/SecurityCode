1efbf80cc2c6dee67c3bb6db13a1b36c627f9c7d
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-16 19:52:39 +0300

    Updated 'logger.js'

--- src/logger.js
+++ src/logger.js
@@ -16,20 +16,28 @@ const logger = createLogger({
     logFormat // Используем кастомный формат
   ),
   transports: [
-    new transports.Console(), // Вывод в консоль
     new transports.File({ filename: "logs/error.log", level: "error" }), // Файл для ошибок
     new transports.File({ filename: "logs/combined.log" }), // Общий файл логов
   ],
 });
 
-// В режиме разработки, добавляем логгирование в консоль
+// Если не в продакшене, используем упрощённый формат для консоли
 if (process.env.NODE_ENV !== "production") {
   logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
+    new transports.Console({
+      format: combine(
+        format.colorize(), // Цветной вывод в режиме разработки
+        format.simple() // Упрощённый вывод
+      ),
+    })
+  );
+} else {
+  // В продакшене используем более подробный вывод
+  logger.add(
+    new transports.Console({
+      format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
     })
   );
 }
 
-// Экспортируем логгер по умолчанию
 export default logger;
