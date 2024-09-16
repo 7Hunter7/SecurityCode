af6c69efb07c377d7337fbd7077660361d043872
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-16 20:58:13 +0300

    Updated 'importCSV.js - addedasync/await

--- src/data/importCSV.js
+++ src/data/importCSV.js
@@ -2,6 +2,8 @@ import fs from "fs";
 import csv from "csv-parser";
 import SIZItem from "../models/SIZItem.js"; // Импорт модели
 
+const isValidDate = (date) => !isNaN(Date.parse(date));
+
 const importCSV = () => {
   const results = [];
   fs.createReadStream("./SIZinventory.csv")
@@ -10,23 +12,34 @@ const importCSV = () => {
       results.push(data);
     })
     .on("end", async () => {
-      for (const row of results) {
-        await SIZItem.create({
+      const promises = results.map((row) => {
+        return SIZItem.create({
           location: row["Местонахождение"],
           type: row["Вид СЗ"],
           voltageClass: row["Класс напряжения СЗ, кВ"],
           szType: row["Тип СЗ"],
           number: row["№ СЗ"],
-          testDate: new Date(row["Дата испытания"]),
-          nextTestDate: new Date(row["Дата следующего испытания"]),
-          lastInspectDate: row["Дата последнего осмотра"]
+          testDate: isValidDate(row["Дата испытания"])
+            ? new Date(row["Дата испытания"])
+            : null,
+          nextTestDate: isValidDate(row["Дата следующего испытания"])
+            ? new Date(row["Дата следующего испытания"])
+            : null,
+          lastInspectDate: isValidDate(row["Дата последнего осмотра"])
             ? new Date(row["Дата последнего осмотра"])
             : null,
           quantity: parseInt(row["Количество"], 10),
           classQuantity: null, // Этот параметр будет рассчитан позже
           note: row["Примечание"],
+        }).catch((err) => {
+          console.error(
+            `Ошибка при создании записи для СИЗ ${row["№ СЗ"]}:`,
+            err
+          );
         });
-      }
+      });
+
+      await Promise.all(promises);
       console.log("Импорт данных завершен.");
     });
 };
