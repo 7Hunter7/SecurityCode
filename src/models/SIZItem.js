54dc99eb5aed5bf9f048455bf598d08fdeaeb627
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-16 19:29:03 +0300

    Added validation data in 'SIZItem.js'

--- src/models/SIZItem.js
+++ src/models/SIZItem.js
@@ -6,14 +6,23 @@ const SIZItem = sequelize.define("SIZItem", {
   location: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      len: [3, 100],
+    },
   },
   type: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      len: [3, 100],
+    },
   },
   voltageClass: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      isIn: [["0,4", "1", "3", "6", "10", "15", "20", "35", "110", "220"]],
+    },
   },
   szType: {
     type: DataTypes.STRING,
@@ -23,6 +32,9 @@ const SIZItem = sequelize.define("SIZItem", {
     type: DataTypes.INTEGER,
     allowNull: false,
     unique: true,
+    validate: {
+      isInt: true,
+    },
   },
   testDate: {
     type: DataTypes.DATE,
@@ -34,15 +46,19 @@ const SIZItem = sequelize.define("SIZItem", {
   },
   lastInspectDate: {
     type: DataTypes.DATE,
-    allowNull: true,
   },
   quantity: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    validate: {
+      min: 1,
+    },
   },
   note: {
     type: DataTypes.STRING,
-    allowNull: true,
+    validate: {
+      len: [0, 255],
+    },
   },
 });
 
