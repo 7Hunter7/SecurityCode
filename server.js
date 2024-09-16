020ab2df9011d93788ac8a676e180af9ddfa8d53
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-16 19:11:14 +0300

    Upgrated server.js for SQLire

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import sequelize from "./src/db.js";
 import sizRoutes from "./src/routes/sizRoutes.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
-import dotenv from "dotenv"; // Подключаем dotenv для работы с переменными окружения
-
-dotenv.config(); // Настраиваем dotenv
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Подключение к MongoDB
-const mongoUri = process.env.MONGO_URI;
-console.log("MONGO_URI:", mongoUri);
-if (!mongoUri) {
-  throw new Error("MONGO_URI is not defined in environment variables");
-}
-
-// Подключение к MongoDB Atlas через переменную окружения
-mongoose
-  .connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// Подключение к базе данных и синхронизация модели
+sequelize
+  .sync()
+  .then(() => {
+    console.log("База данных синхронизирована");
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Ошибка подключения к MongoDB:", err));
+  .catch((err) => {
+    console.error("Ошибка синхронизации базы данных:", err);
+  });
 
 // Подключение маршрутов
 app.use("/api/siz-items", sizRoutes);
