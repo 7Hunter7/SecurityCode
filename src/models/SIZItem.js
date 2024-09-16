fcc256d1cb51be356c8e75bdbfe5f66f7f42cbf9
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-16 19:06:59 +0300

    Upgrated SIZItem.js for SQLire

--- src/models/SIZItem.js
+++ src/models/SIZItem.js
@@ -1,42 +1,49 @@
-import mongoose from "mongoose";
+// src/models/SIZItem.js
+import { DataTypes } from "sequelize";
+import sequelize from "../db.js";
 
-// Определение схемы и модели с валидацией
-const SIZItemSchema = new mongoose.Schema({
-  location: { type: String, required: true, minlength: 3, maxlength: 100 },
-  type: { type: String, required: true, minlength: 3, maxlength: 100 },
-  voltageClass: {
-    type: String,
-    required: true,
-    enum: [
-      "0,4",
-      "1",
-      "3",
-      "6",
-      "10",
-      "15",
-      "20",
-      "35",
-      "110",
-      "150",
-      "220",
-      "330",
-      "500",
-      "750",
-      "1150",
-    ],
+const SIZItem = sequelize.define("SIZItem", {
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  szType: { type: String, required: true },
-  number: { type: Number, required: true, unique: true }, // Уникальный номер
-  testDate: { type: Date, required: true },
-  nextTestDate: { type: Date, required: true },
-  lastInspectDate: { type: Date, default: Date.now },
-  quantity: { type: Number, required: true, min: 1 },
-  note: { type: String, maxlength: 255 },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  voltageClass: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  szType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  testDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  nextTestDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  lastInspectDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  note: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
 });
-// SIZItemSchema.index({ number: 1 }, { unique: true }); // Создание уникального индекса
-// Команда для применения индексов:
-// SIZItem.syncIndexes();
 
-// Экспорт модели по умолчанию
-const SIZItem = mongoose.model("SIZItem", SIZItemSchema);
 export default SIZItem;
