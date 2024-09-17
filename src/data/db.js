ff17aae14a9d45a4107ebb87ad797730292dd76e
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-17 07:56:53 +0300

    Added checking database connection in 'data/db.js'

--- src/data/db.js
+++ src/data/db.js
@@ -3,6 +3,13 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite", // Файл базы данных
+  logging: false, // Выключить логи, если не нужны
 });
+
+// Проверка соединения с БД
+sequelize
+  .authenticate()
+  .then(() => console.log("Соединение с базой данных успешно установлено."))
+  .catch((err) => console.error("Невозможно подключиться к базе данных:", err));
 
 export default sequelize;
