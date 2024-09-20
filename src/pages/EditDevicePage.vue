7703e79bf30e1078a52555cb2b79bb6cd8917a11
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-20 08:37:47 +0300

    Updated  component 'EditDevicePage' - Added usage 'InputField' and 'DateCalculations'

--- src/pages/EditDevicePage.vue
+++ src/pages/EditDevicePage.vue
@@ -1,167 +1,67 @@
 <template>
   <div class="edit-siz-page">
     <h1>Редактировать СИЗ</h1>
+
     <form @submit.prevent="submitForm">
-      <!-- Местонахождение -->
-      <div class="form-group">
-        <label for="location">Местонахождение:</label>
-        <div>
-          <select v-model="siz.location" id="location" required>
-            <option value="">Выберите местонахождение</option>
-            <option
-              v-for="location in locations"
-              :key="location"
-              :value="location"
-            >
-              {{ location }}
-            </option>
-          </select>
-          <input
-            v-if="siz.location === 'new'"
-            v-model="newLocation"
-            type="text"
-            placeholder="Добавить новое местонахождение"
-          />
-        </div>
-      </div>
+      <!-- Используем компонент InputField для каждого поля -->
+      <InputField
+        label="Местонахождение"
+        :options="locations"
+        v-model="siz.location"
+        :newValue.sync="newLocation"
+      />
+      <InputField
+        label="Вид СЗ"
+        :options="types"
+        v-model="siz.type"
+        :newValue.sync="newType"
+      />
+      <InputField
+        label="Класс напряжения (кВ)"
+        :options="voltageClasses"
+        v-model="siz.voltageClass"
+        :newValue.sync="newVoltageClass"
+      />
+      <InputField
+        label="Тип СЗ"
+        :options="szTypes"
+        v-model="siz.szType"
+        :newValue.sync="newSzType"
+      />
+      <InputField label="№ СЗ" type="text" v-model="siz.number" required />
+      <InputField
+        label="Дата испытания"
+        type="date"
+        v-model="siz.testDate"
+        @change="calculateNextTestDate"
+        required
+      />
+      <InputField
+        label="Дата следующего испытания"
+        type="date"
+        v-model="siz.nextTestDate"
+        required
+      />
+      <InputField
+        label="Дата последнего осмотра"
+        type="date"
+        v-model="siz.lastInspectDate"
+      />
+      <InputField
+        label="Количество"
+        type="number"
+        v-model="siz.quantity"
+        min="1"
+        required
+      />
+      <InputField
+        label="Примечания"
+        :options="notes"
+        v-model="siz.note"
+        :newValue.sync="newNote"
+      />
 
-      <!-- Вид СЗ -->
-      <div class="form-group">
-        <label for="type">Вид СЗ:</label>
-        <div>
-          <select v-model="siz.type" id="type" required>
-            <option value="">Выберите вид СЗ</option>
-            <option v-for="type in types" :key="type" :value="type">
-              {{ type }}
-            </option>
-          </select>
-          <input
-            v-if="siz.type === 'new'"
-            v-model="newType"
-            type="text"
-            placeholder="Добавить новый вид СЗ"
-          />
-        </div>
-      </div>
-
-      <!-- Класс напряжения (кВ) -->
-      <div class="form-group">
-        <label for="voltageClass">Класс напряжения (кВ):</label>
-        <div>
-          <select v-model="siz.voltageClass" id="voltageClass" required>
-            <option value="">Выберите класс напряжения</option>
-            <option
-              v-for="voltage in voltageClasses"
-              :key="voltage"
-              :value="voltage"
-            >
-              {{ voltage }} кВ
-            </option>
-          </select>
-          <input
-            v-if="siz.voltageClass === 'new'"
-            v-model="newVoltageClass"
-            type="text"
-            placeholder="Добавить новый класс напряжения"
-          />
-        </div>
-      </div>
-
-      <!-- Тип СЗ -->
-      <div class="form-group">
-        <label for="szType">Тип СЗ:</label>
-        <div>
-          <select v-model="siz.szType" id="szType" required>
-            <option value="">Выберите тип СЗ</option>
-            <option v-for="szType in szTypes" :key="szType" :value="szType">
-              {{ szType }}
-            </option>
-          </select>
-          <input
-            v-if="siz.szType === 'new'"
-            v-model="newSzType"
-            type="text"
-            placeholder="Добавить новый тип СЗ"
-          />
-        </div>
-      </div>
-
-      <!-- № СЗ -->
-      <div class="form-group">
-        <label for="number">№ СЗ:</label>
-        <input
-          v-model="siz.number"
-          type="text"
-          id="number"
-          placeholder="Введите номер СЗ"
-          required
-        />
-      </div>
-
-      <!-- Дата испытания -->
-      <div class="form-group">
-        <label for="testDate">Дата испытания:</label>
-        <input
-          type="date"
-          v-model="siz.testDate"
-          id="testDate"
-          @change="setNextTestDate"
-          required
-        />
-      </div>
-
-      <!-- Дата следующего испытания -->
-      <div class="form-group">
-        <label for="nextTestDate">Дата следующего испытания:</label>
-        <input
-          type="date"
-          v-model="siz.nextTestDate"
-          id="nextTestDate"
-          required
-        />
-      </div>
-
-      <!-- Дата последнего осмотра -->
-      <div class="form-group">
-        <label for="lastInspectDate">Дата последнего осмотра:</label>
-        <input type="date" v-model="siz.lastInspectDate" id="lastInspectDate" />
-      </div>
-
-      <!-- Количество -->
-      <div class="form-group">
-        <label for="quantity">Количество:</label>
-        <input
-          v-model="siz.quantity"
-          type="number"
-          id="quantity"
-          min="1"
-          placeholder="Введите количество"
-          value="1"
-          required
-        />
-      </div>
-
-      <!-- Примечания -->
-      <div class="form-group">
-        <label for="note">Примечания:</label>
-        <div>
-          <select v-model="siz.note" id="note">
-            <option value="">Выберите примечание</option>
-            <option v-for="note in notes" :key="note" :value="note">
-              {{ note }}
-            </option>
-          </select>
-          <input
-            v-if="siz.note === 'new'"
-            v-model="newNote"
-            type="text"
-            placeholder="Добавить новое примечание"
-            :readonly="true"
-          />
-        </div>
-      </div>
-
-      <!-- Кнопка сохранения -->
+      <!-- Кнопка для сохранения изменений -->
       <div class="form-actions">
         <button type="submit">Сохранить изменения</button>
       </div>
@@ -170,10 +70,16 @@
 </template>
 
 <script>
+import InputField from "../components/InputField.vue";
+import DateCalculations from "../components/DateCalculations.vue";
 import { mapState } from "vuex";
 
 export default {
   name: "EditDevicePage",
+  components: {
+    InputField,
+    DateCalculations,
+  },
   data() {
     return {
       siz: {
@@ -196,29 +102,34 @@ export default {
     };
   },
   computed: {
-    ...mapState([
-      "locations",
-      "types",
-      "voltageClasses",
-      "szTypes",
-      "notes",
-      "sizItems",
-    ]),
+    ...mapState(["locations", "types", "voltageClasses", "szTypes", "notes"]),
   },
   mounted() {
     if (this.$route.params.id) {
-      const existingSIZ = this.sizItems.find(
+      const existingSIZ = this.$store.getters.getSizItems.find(
         (item) => item.id === +this.$route.params.id
       );
       if (existingSIZ) {
-        this.siz = { ...existingSIZ }; // Загружаем данные для редактирования
+        this.siz = { ...existingSIZ };
       }
     }
   },
   methods: {
     submitForm() {
-      this.$emit("updateSIZ", this.siz); // Отправляем событие для обновления
+      if (this.newLocation) this.siz.location = this.newLocation;
+      if (this.newType) this.siz.type = this.newType;
+      if (this.newVoltageClass) this.siz.voltageClass = this.newVoltageClass;
+      if (this.newSzType) this.siz.szType = this.newSzType;
+      if (this.newNote) this.siz.note = this.newNote;
+
+      this.$emit("updateSIZ", this.siz);
       this.$router.push("/security-device");
+    },
+    calculateNextTestDate() {
+      this.siz.nextTestDate = this.$refs.dateCalculations.setNextTestDate(
+        this.siz.testDate,
+        this.siz.type
+      );
     },
   },
 };
