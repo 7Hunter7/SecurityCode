c17d6f5e06243b2fe7d774a1723e38985a28fd91
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-22 16:33:05 +0300

    Updated component importCSV.js - Import and use functions calculateQuantityByClass and isValidDate

--- src/data/importCSV.js
+++ src/data/importCSV.js
@@ -1,16 +1,13 @@
 import fs from "fs";
 import csv from "csv-parser";
-import SIZItem from "../models/SIZItem.js"; // Импорт модели
-
-const isValidDate = (date) => {
-  return !isNaN(Date.parse(date));
-};
+import SIZItem from "../models/SIZItem.js";
+import { calculateQuantityByClass } from "./utils.js"; // Импорт функции для подсчета количества по классам
+import { isValidDate } from "./dateUtils.js"; // Импорт функции валидации даты
 
 const importCSV = async () => {
   const results = [];
   const quantityByClass = {};
 
-  // Чтение и обработка данных из CSV
   fs.createReadStream("./SIZinventory.csv")
     .pipe(csv())
     .on("data", (data) => {
@@ -18,18 +15,8 @@ const importCSV = async () => {
     })
     .on("end", async () => {
       for (const row of results) {
-        // Создаем уникальный ключ для подсчета количества по классам
-        const key = `${row["Вид СЗ"]}_${row["Класс напряжения СЗ, кВ"]}_${row["Местонахождение"]}`;
+        const classQuantity = calculateQuantityByClass(quantityByClass, row);
 
-        // Инициализируем количество для данного класса, если его еще нет
-        if (!quantityByClass[key]) {
-          quantityByClass[key] = 0;
-        }
-
-        // Добавляем количество из текущей строки
-        quantityByClass[key] += parseInt(row["Количество"], 10);
-
-        // Проверка дат
         const testDate = isValidDate(row["Дата испытания"])
           ? new Date(row["Дата испытания"])
           : null;
@@ -40,15 +27,13 @@ const importCSV = async () => {
           ? new Date(row["Дата последнего осмотра"])
           : null;
 
-        // Если тестовая дата или дата следующего испытания некорректны, пропускаем запись
         if (!testDate || !nextTestDate) {
           console.log(
             `Ошибка в строке с номером СЗ ${row["№ СЗ"]}: некорректные даты испытания или следующего испытания.`
           );
-          continue; // Пропускаем некорректные записи
+          continue;
         }
 
-        // Сохраняем каждую запись в базу данных
         await SIZItem.create({
           location: row["Местонахождение"],
           type: row["Вид СЗ"],
@@ -57,9 +42,9 @@ const importCSV = async () => {
           number: row["№ СЗ"],
           testDate: testDate,
           nextTestDate: nextTestDate,
-          lastInspectDate: lastInspectDate, // Если дата осмотра не указана, она останется null
+          lastInspectDate: lastInspectDate,
           quantity: parseInt(row["Количество"], 10),
-          classQuantity: quantityByClass[key], // Количество по классу напряжения
+          classQuantity: classQuantity,
           note: row["Примечание"],
         });
       }
