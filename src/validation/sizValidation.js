50ea5d73b5dd7c4f545c49654ea786bf81cc6c62
Hunter <i.am.hunter.777.ih@gmail.com>
2024-09-28 17:22:42 +0300

    Updated validation/sizValidation.js - Validation schema code matches the data model

--- src/validation/sizValidation.js
+++ src/validation/sizValidation.js
@@ -2,16 +2,16 @@ import Joi from "joi";
 
 // Схема валидации Joi
 export const sizItemValidationSchema = Joi.object({
-  location: Joi.string().min(3).max(100).required(),
-  type: Joi.string().min(3).max(100).required(),
+  location: Joi.string().min(3).max(255).required(),
+  type: Joi.string().min(3).max(255).required(),
   voltageClass: Joi.string()
     .valid("0,4", "1", "3", "6", "10", "15", "20", "35", "110", "220")
     .required(),
-  szType: Joi.string().required(),
-  number: Joi.number().integer().required(),
+  szType: Joi.string().allow(""),
+  number: Joi.string().required(),
   testDate: Joi.date().required(),
   nextTestDate: Joi.date().required(),
-  lastInspectDate: Joi.date(),
+  lastInspectDate: Joi.date().optional(), // Поле не обязательное
   quantity: Joi.number().integer().min(1).required(),
-  note: Joi.string().max(255),
+  note: Joi.string().max(255).allow(""),
 });
