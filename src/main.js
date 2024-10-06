a7eddd3134d58575724a8a6edd5033e34346a323
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-06 09:26:51 +0300

    Updated 'main.js' - Added usage  library 'vue-toastification' to show pop-up notifications

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.use(Toast, {
+  position: POSITION.TOP_RIGHT, // Позиция для всплывающих уведомлений
+  timeout: 5000, // Время показа уведомления
+});
+
 app.mount("#app");
