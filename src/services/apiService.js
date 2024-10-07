cfa9c055521ce4ced59c333482ea24499816d3eb
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-07 12:48:09 +0300

    Updated 'services/apiService.js' - Updatde API_URL and functions

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const API_URL = "/api/siz-items";
+const API_URL = "/api"; // Используем прокси
 
 // Получение всех записей СИЗ
-export const getSIZItems = () => axios.get(API_URL);
+export const getSIZItems = () => axios.get(`${API_URL}/siz-items`);
 
 // Создание новой записи СИЗ
-export const createSIZItem = (item) => axios.post(API_URL, item);
+export const createSIZItem = (item) => axios.post(`${API_URL}/siz-items`, item);
 
 // Обновление записи СИЗ
 export const updateSIZItem = (id, updatedItem) =>
-  axios.put(`${API_URL}/${id}`, updatedItem);
+  axios.put(`${API_URL}/siz-items/${id}`, updatedItem);
 
 // Удаление записи СИЗ
-export const deleteSIZItem = (id) => axios.delete(`${API_URL}/${id}`);
+export const deleteSIZItem = (id) => axios.delete(`${API_URL}/siz-items/${id}`);
