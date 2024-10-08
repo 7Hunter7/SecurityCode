5216018014229f2da4719ac8d1232ddf587b0e21
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-08 08:18:14 +0300

    Updated 'vite.config.js' -  Updated proxy - rewrite

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
