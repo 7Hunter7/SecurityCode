ac20872903f4f19f52dccb0b1f4a0a40cea19737
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-09 08:14:18 +0300

    Updated component 'HistoryPage' - Added table 'History of changes'

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -1,6 +1,28 @@
 <template>
   <main>
-    <h1>Это страница истории</h1>
+    <h1>История изменений</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Дата</th>
+          <th>Действие</th>
+          <th>Тип</th>
+          <th>ID</th>
+          <th>Пользователь</th>
+          <th>Детали</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in history" :key="item.id">
+          <td>{{ new Date(item.timestamp).toLocaleString() }}</td>
+          <td>{{ item.action }}</td>
+          <td>{{ item.entityType }}</td>
+          <td>{{ item.entityId }}</td>
+          <td>{{ item.userId || "Неизвестно" }}</td>
+          <td>{{ JSON.stringify(item.details) }}</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
