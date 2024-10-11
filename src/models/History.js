8500c47af14f0f8bbc086ee2b99c4652f8e66f89
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-11 07:23:10 +0300

    Updated 'models/History.js'

--- src/models/History.js
+++ src/models/History.js
@@ -4,7 +4,7 @@ import sequelize from "../data/db.js";
 const History = sequelize.define("History", {
   id: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    primaryKey: true,
   },
   timestamp: {
     type: DataTypes.DATE,
