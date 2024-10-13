2a373c100979e91eb059866d5c66639fc278ebcb
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-13 08:32:18 +0300

    Updated 'validation/sizValidation.js'

--- src/validation/sizValidation.js
+++ src/validation/sizValidation.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-// Схема валидации Joi
+// Joi схема валидации
 export const sizItemValidationSchema = Joi.object({
   location: Joi.string().min(3).max(255).required(),
   type: Joi.string().min(3).max(255).required(),
@@ -8,15 +8,15 @@ export const sizItemValidationSchema = Joi.object({
     .valid("0,4", "1", "3", "6", "10", "15", "20", "35", "110", "220")
     .required(),
   szType: Joi.string().allow(""),
-  number: Joi.number().integer().required(),
+  number: Joi.number().integer().required(), // Отправляется число
   testDate: Joi.string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/) // Проверка формата даты (yyyy-MM-dd)
-    .required(),
+    .regex(/^\d{4}-\d{2}-\d{2}$/)
+    .required(), // Строгая проверка формата даты
   nextTestDate: Joi.string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/) // Проверка формата даты (yyyy-MM-dd)
+    .regex(/^\d{4}-\d{2}-\d{2}$/)
     .required(),
   lastInspectDate: Joi.string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/) // Проверка формата даты (yyyy-MM-dd)
+    .regex(/^\d{4}-\d{2}-\d{2}$/)
     .optional(),
   quantity: Joi.number().integer().min(1).required(),
   inspectionResult: Joi.string().max(255).allow(""),
