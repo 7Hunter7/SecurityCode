fdc83438191adf383ade0906bc89b2b6b2326c14
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-15 08:32:31 +0300

    Updated 'validation/sizValidation.js' - Added usage import 'PZ_TYPES' and 'voltageClasses' from constants

--- src/validation/sizValidation.js
+++ src/validation/sizValidation.js
@@ -1,20 +1,29 @@
 import Joi from "joi";
+import { PZ_TYPES, voltageClasses } from "../constants/constants.js"; // Импортируем массив ПЗ
 
 // Joi схема валидации
 export const sizItemValidationSchema = Joi.object({
   location: Joi.string().min(3).max(255).required(),
   type: Joi.string().min(3).max(255).required(),
   voltageClass: Joi.string()
-    .valid("0,4", "1", "3", "6", "10", "15", "20", "35", "110", "220")
+    .valid(...voltageClasses)
     .required(),
   szType: Joi.string().allow(""),
   number: Joi.number().integer().required(), // Отправляется число
-  testDate: Joi.string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/)
-    .required(), // Строгая проверка формата даты
-  nextTestDate: Joi.string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/)
-    .required(),
+  testDate: Joi.alternatives().conditional("type", {
+    is: Joi.valid(...PZ_TYPES),
+    then: Joi.string().allow("").optional(), // Необязательное поле
+    otherwise: Joi.string()
+      .regex(/^\d{4}-\d{2}-\d{2}$/)
+      .required(), // Обязательное поле
+  }),
+  nextTestDate: Joi.alternatives().conditional("type", {
+    is: Joi.valid(...PZ_TYPES),
+    then: Joi.string().allow("").optional(), // Необязательное поле
+    otherwise: Joi.string()
+      .regex(/^\d{4}-\d{2}-\d{2}$/)
+      .required(), // Обязательное поле
+  }),
   lastInspectDate: Joi.string()
     .regex(/^\d{4}-\d{2}-\d{2}$/)
     .optional(),
