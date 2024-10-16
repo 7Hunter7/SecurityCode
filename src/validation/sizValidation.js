caa0763b40dc6888c86ee74fb8370dd95be1f928
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-16 07:11:17 +0300

    Updated 'validation/sizValidation.js' - Added usage import 'VOLTAGE_CLASSES' and others improvements

--- src/validation/sizValidation.js
+++ src/validation/sizValidation.js
@@ -1,29 +1,29 @@
 import Joi from "joi";
-import { PZ_TYPES, voltageClasses } from "../constants/constants.js"; // Импортируем массив ПЗ
+import { PZ_TYPES, VOLTAGE_CLASSES } from "../constants/constants.js";
 
 // Joi схема валидации
 export const sizItemValidationSchema = Joi.object({
   location: Joi.string().min(3).max(255).required(),
   type: Joi.string().min(3).max(255).required(),
   voltageClass: Joi.string()
-    .valid(...voltageClasses)
+    .valid(...VOLTAGE_CLASSES) // Используем константу для проверки классов напряжений
     .required(),
   szType: Joi.string().allow(""),
-  number: Joi.number().integer().required(), // Отправляется число
-  testDate: Joi.alternatives().conditional("type", {
-    is: Joi.valid(...PZ_TYPES),
-    then: Joi.string().allow("").optional(), // Необязательное поле
-    otherwise: Joi.string()
-      .regex(/^\d{4}-\d{2}-\d{2}$/)
-      .required(), // Обязательное поле
-  }),
-  nextTestDate: Joi.alternatives().conditional("type", {
-    is: Joi.valid(...PZ_TYPES),
-    then: Joi.string().allow("").optional(), // Необязательное поле
-    otherwise: Joi.string()
-      .regex(/^\d{4}-\d{2}-\d{2}$/)
-      .required(), // Обязательное поле
-  }),
+  number: Joi.number().integer().required(), // Номер СИЗ
+  testDate: Joi.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/) // Строгая проверка даты
+    .when("type", {
+      not: Joi.valid(...PZ_TYPES), // Если тип не ПЗ, поле обязательно
+      then: Joi.required(),
+      otherwise: Joi.optional(),
+    }),
+  nextTestDate: Joi.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/)
+    .when("type", {
+      not: Joi.valid(...PZ_TYPES),
+      then: Joi.required(),
+      otherwise: Joi.optional(),
+    }),
   lastInspectDate: Joi.string()
     .regex(/^\d{4}-\d{2}-\d{2}$/)
     .optional(),
