0922869b6bcb09b55dd85ee6788174e07e3006e4
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-16 08:43:01 +0300

    Updated component 'SecurityDevicePage' - Deleted a second argument to the formatDate method, which checks the SIZ type

--- src/pages/SecurityDevicePage.vue
+++ src/pages/SecurityDevicePage.vue
@@ -37,12 +37,12 @@
           <td class="voltageClass">{{ item.voltageClass }}</td>
           <td class="szType">{{ item.szType }}</td>
           <td class="number">{{ item.number }}</td>
-          <td class="testDate">{{ formatDate(item.testDate, item.type) }}</td>
+          <td class="testDate">{{ formatDate(item.testDate) }}</td>
           <td class="nextTestDate">
-            {{ formatDate(item.nextTestDate, item.type) }}
+            {{ formatDate(item.nextTestDate) }}
           </td>
           <td class="lastInspectDate">
-            {{ formatDate(item.lastInspectDate, item.type) }}
+            {{ formatDate(item.lastInspectDate) }}
           </td>
           <td class="quantity">{{ item.quantity }}</td>
           <td class="inspection-result">{{ item.inspectionResult }}</td>
@@ -150,7 +150,7 @@ export default {
 
     // Форматирование дат
     formatDate(date, type) {
-      if (!date || PZ_TYPES.includes(type)) return "—"; // Если дата не указана или тип СЗ относится к PZ_TYPES, возвращаем "—"
+      if (!date || date === "") return "—";
       const options = { year: "numeric", month: "2-digit", day: "2-digit" };
       return new Date(date).toLocaleDateString("ru-RU", options);
     },
