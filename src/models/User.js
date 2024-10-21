7e8be5c05a8868c120472bdf47f3f2dcbcacdef3
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-21 19:59:20 +0300

    Added new model in 'models/User.js'

---
+++ src/models/User.js
@@ -0,0 +1,60 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../data/db.js";
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  middleName: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  department: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  profilePhoto: {
+    type: DataTypes.STRING, // Ссылка на фото
+    allowNull: true,
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  phoneVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  notifications: {
+    type: DataTypes.JSON, // Хранит настройки уведомлений
+    defaultValue: {
+      email: false,
+      phone: false,
+    },
+  },
+});
+
+export default User;
