aa9e0261bb55c3a76ae9ccd02e7dc7578ecd8b32
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 10:36:47 +0300

    Updated 'models/User.js' - Added Password hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../data/db.js";
+import bcrypt from "bcrypt";
 
 const User = sequelize.define("User", {
   id: {
@@ -10,6 +11,7 @@ const User = sequelize.define("User", {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   password: {
     type: DataTypes.STRING,
@@ -71,5 +73,26 @@ const User = sequelize.define("User", {
     },
   },
 });
+
+// Хэширование пароля перед сохранением нового пользователя
+User.beforeCreate(async (user) => {
+  if (user.password) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+});
+
+// Хэширование пароля перед обновлением, если пароль был изменён
+User.beforeUpdate(async (user) => {
+  if (user.password && user.changed("password")) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+});
+
+// Метод для сравнения пароля
+User.prototype.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export default User;
