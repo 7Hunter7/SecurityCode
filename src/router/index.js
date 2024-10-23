0cbbc06661aa83acb3a567706670cd8ff72102da
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 07:02:55 +0300

    Updated 'routesr/index.js' - Updated path to 'Profile'

--- src/router/index.js
+++ src/router/index.js
@@ -41,7 +41,7 @@ const router = createRouter({
       component: () => import("../pages/RegistrationPage.vue"),
     },
     {
-      path: "/profile:id?", // Маршрут профиля пользователя
+      path: "/profile/:id?", // Маршрут профиля пользователя
       name: "Profile",
       component: () => import("../pages/ProfilePage.vue"),
     },
