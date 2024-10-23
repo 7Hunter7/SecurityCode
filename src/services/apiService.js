2f10239099ddd5ab4b71395eeea315530f858197
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 11:45:13 +0300

    Updated 'services/apiService.js' - Added 'API_URL_USERS', getUsers, createUser, updateUser and deleteUser

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -14,3 +14,18 @@ export const updateSIZItem = (id, updatedItem) =>
 
 // Удаление записи СИЗ
 export const deleteSIZItem = (id) => axios.delete(`${API_URL}/${id}`);
+
+const API_URL_USERS = "http://localhost:3000/api/users";
+
+// Получение всех пользователей (администратор)
+export const getUsers = () => axios.get(API_URL_USERS);
+
+// Создание нового пользователя (администратор)
+export const createUser = (user) => axios.post(API_URL_USERS, user);
+
+// Обновление данных пользователя (администратор)
+export const updateUser = (id, updatedUser) =>
+  axios.put(`${API_URL_USERS}/${id}`, updatedUser);
+
+// Удаление пользователя (администратор)
+export const deleteUser = (id) => axios.delete(`${API_URL_USERS}/${id}`);
