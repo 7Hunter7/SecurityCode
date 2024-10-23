afa3ff0dae5884ee93f1f1e72c7fdb9340e0fc99
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 08:01:15 +0300

    Updated new component 'ScrollTopButton'

--- src/components/ScrollTopButton.vue
+++ src/components/ScrollTopButton.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="scroll-to-top" v-if="showButton" @click="scrollToTop">⬆️</div>
+  <div class="scroll-to-top" v-if="showButton" @click="scrollToTop">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 -4 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Lager_14" data-name="Lager 14" transform="translate(-6 -10)">
+        <path
+          id="Path_16"
+          data-name="Path 16"
+          d="M15.319,15.909a1.97,1.97,0,0,1,.732-.278,1,1,0,0,1,.679.278l5.732,5.517a2.116,2.116,0,0,0,2.887.01l.03-.028a1.958,1.958,0,0,0,.008-2.854l-8.613-8.267A1.077,1.077,0,0,0,16.051,10a2.115,2.115,0,0,0-.775.287L6.611,18.554a1.959,1.959,0,0,0,.012,2.854l.036.028a2.134,2.134,0,0,0,2.9-.01Z"
+          fill="#fff"
+        />
+      </g>
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -20,7 +36,7 @@ export default {
     window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
-    // Если прокрутка превышает заданное значение, кнопка становится видимой.
+    // Появление кнопки при прокрутке более 300px
     handleScroll() {
       const scrollPosition = window.scrollY;
       this.showButton = scrollPosition > 300;
@@ -60,7 +76,7 @@ export default {
   opacity: 1;
 }
 .scroll-to-top:active {
-  transform: scale(0.95);
+  transform: scale(0.95); /* Эффект сжатия при клике */
 }
 /* Поддержка адаптивности  */
 @media (max-width: 768px) {
