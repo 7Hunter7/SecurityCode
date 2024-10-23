96bb05162481528fd03ab4842827450208258fb6
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 10:57:13 +0300

    Updated 'routes/adminUserRoutes.js' - Added import and usage 'authenticateToke' and ' authorizeRole'

--- src/routes/adminUserRoutes.js
+++ src/routes/adminUserRoutes.js
@@ -1,25 +1,30 @@
 import express from "express";
 import User from "../models/User.js";
-import { authorize } from "../middlewares/authorize.js";
+import { authenticateToken, authorizeRole } from "../middlewares/authorize.js";
 import { userValidationSchema } from "../validation/userValidation.js"; // Импорт схемы валидации
 import logger from "../logger.js";
 
 const router = express.Router();
 
-// Получение всех пользователей
-router.get("/", authorize(["admin"]), async (req, res) => {
-  try {
-    const users = await User.findAll();
-    logger.info("Все пользователи успешно получены");
-    res.json(users);
-  } catch (error) {
-    logger.error(`Ошибка получения пользователей: ${error.message}`);
-    res.status(500).json({ message: "Ошибка получения пользователей" });
+// Защищённый маршрут для получения всех пользователей (только администратор)
+router.get(
+  "/users",
+  authenticateToken,
+  authorizeRole(["admin"]),
+  async (req, res) => {
+    try {
+      const users = await User.findAll();
+      logger.info("Все пользователи успешно получены");
+      res.json(users);
+    } catch (error) {
+      logger.error(`Ошибка получения пользователей: ${error.message}`);
+      res.status(500).json({ message: "Ошибка получения пользователей" });
+    }
   }
-});
+);
 
 // Добавление нового пользователя
-router.post("/", authorize(["admin"]), async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   // Валидация данных пользователя перед созданием
   const { error } = userValidationSchema.validate(req.body);
   if (error) {
@@ -47,8 +52,8 @@ router.post("/", authorize(["admin"]), async (req, res, next) => {
   }
 });
 
-// Обновление данных пользователя
-router.put("/:id", authorize(["admin"]), async (req, res, next) => {
+// Защищённый маршрут для обновления данных пользователя (администратор или сам пользователь)
+router.put("/users/:id", authenticateToken, async (req, res, next) => {
   // Валидация данных пользователя перед обновлением
   const { error } = userValidationSchema.validate(req.body);
   if (error) {
@@ -58,18 +63,19 @@ router.put("/:id", authorize(["admin"]), async (req, res, next) => {
     return res.status(400).json({ message: error.details[0].message });
   }
 
+  const userId = req.params.id;
+  // Разрешаем администратору или самому пользователю обновлять данные
+  if (req.user.role !== "admin" && req.user.id !== parseInt(userId)) {
+    return res
+      .status(403)
+      .json({ message: "У вас нет прав для редактирования этих данных" });
+  }
   try {
-    const user = await User.findByPk(req.params.id);
+    const user = await User.findByPk(userId);
     if (user) {
-      const { role, firstName, lastName, email, phone } = req.body;
-      user.role = role;
-      user.firstName = firstName;
-      user.lastName = lastName;
-      user.email = email;
-      user.phone = phone;
-      await user.save();
-      logger.info(`Пользователь с ID: ${user.id} успешно обновлен`);
+      await user.update(req.body);
       res.json(user);
+      logger.info(`Данные пользователя с ID: ${user.id} успешно обновлены`);
     } else {
       res.status(404).json({ message: "Пользователь не найден" });
       logger.warn(`Пользователь с ID: ${req.params.id} не найден`);
@@ -80,22 +86,27 @@ router.put("/:id", authorize(["admin"]), async (req, res, next) => {
   }
 });
 
-// Удаление пользователя
-router.delete("/:id", authorize(["admin"]), async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-    if (user) {
-      await user.destroy();
-      logger.info(`Пользователь с ID: ${user.id} успешно удален`);
-      res.json({ message: "Пользователь удален" });
-    } else {
-      res.status(404).json({ message: "Пользователь не найден" });
-      logger.warn(`Пользователь с ID:${user.id} не найден`);
+// Защищённый маршрут для удаления пользователя
+router.delete(
+  "/:id",
+  authenticateToken,
+  authorize(["admin"]),
+  async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (user) {
+        await user.destroy();
+        logger.info(`Пользователь с ID: ${user.id} успешно удален`);
+        res.json({ message: "Пользователь удален" });
+      } else {
+        res.status(404).json({ message: "Пользователь не найден" });
+        logger.warn(`Пользователь с ID:${user.id} не найден`);
+      }
+    } catch (error) {
+      logger.error(`Ошибка удаления пользователя: ${error.message}`);
+      next(error);
     }
-  } catch (error) {
-    logger.error(`Ошибка удаления пользователя: ${error.message}`);
-    next(error);
   }
-});
+);
 
 export default router;
