8aabce50a4e5086437c75106d0613cbd43c0dfd6
Hunter <i.am.hunter.777.ih@gmail.com>
2024-10-23 10:21:04 +0300

    Updated 'validation/userValidation.js' - Added: username, password, role; and other improvements

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -2,19 +2,22 @@ import Joi from "joi";
 
 // Joi схема валидации
 export const userValidationSchema = Joi.object({
-  firstName: Joi.string().min(2).max(50).required(),
-  lastName: Joi.string().min(2).max(50).required(),
-  middleName: Joi.string().allow(null, ""),
-  department: Joi.string().max(100).allow(null, ""),
+  username: Joi.string().min(3).max(30).required(),
+  password: Joi.string().min(6).max(100).required(),
+  role: Joi.string().valid("user", "advanced_user", "admin").default("user"),
+  firstName: Joi.string().min(1).max(100).required(),
+  lastName: Joi.string().min(1).max(100).required(),
+  middleName: Joi.string().min(0).max(100).optional(),
+  department: Joi.string().min(0).max(255).optional(),
   email: Joi.string().email().required(),
   phone: Joi.string()
-    .regex(/^\+\d{10,15}$/)
-    .required(), // Номер телефона в формате +123456789
-  profilePhoto: Joi.string().allow(null, ""),
-  emailVerified: Joi.boolean(),
-  phoneVerified: Joi.boolean(),
+    .pattern(/^[0-9]{10,15}$/)
+    .required(),
+  profilePhoto: Joi.string().optional(),
+  emailVerified: Joi.boolean().default(false),
+  phoneVerified: Joi.boolean().default(false),
   notifications: Joi.object({
     email: Joi.boolean().default(false),
     phone: Joi.boolean().default(false),
-  }),
+  }).default(),
 });
